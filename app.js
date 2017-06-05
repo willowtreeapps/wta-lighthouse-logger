0b27388cea85e88da39c30dc67d2753c724e1ea2
two7sclash <james.fishwick@willowtreeapps.com>
2017-06-05 16:27:26 -0400

    adding mongo

---
+++ app.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://heroku_9sb7jt3f:NxuovgQ2R2@ds031607.mlab.com:31607/heroku_9sb7jt3f';
+// Use connect method to connect to the Server
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
